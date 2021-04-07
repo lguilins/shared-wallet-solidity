8c7f475b61704f06087a6ec3a8fc05f191158993
Luiz Guilherme Lins <lins.luizguilherme@gmail.com>
2021-04-07 19:06:52 -0300

    implement basic functionalities

---
+++ SharedWallet.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SimpleWallet is Ownable {
+    function withdrawMoney(address payable _to, uint256 _amount)
+        public
+        onlyOwner
+    {
+        _to.transfer(_amount);
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
+}
