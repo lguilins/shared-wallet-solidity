dd5d87482032a56a96cbd844fa4df2295062fa22
Luiz Guilherme Lins <lins.luizguilherme@gmail.com>
2021-04-08 13:02:38 -0300

    add events for the simple wallet smart contract

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -38,6 +38,9 @@ contract Allowance is Ownable {
 }
 
 contract SimpleWallet is Allowance {
+    event MoneySent(address indexed _beneficiary, uint256 _amount);
+    event MoneyReceived(address indexed _from, uint256 _amount);
+
     function withdrawMoney(address payable _to, uint256 _amount)
         public
         ownerOrAllowed(_amount)
@@ -49,10 +52,11 @@ contract SimpleWallet is Allowance {
         if (!isOwner()) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
-    fallback() external payable {}
-
-    receive() external payable {}
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 }
