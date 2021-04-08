9f87235118ffffd8cdd70e9bf074f0add58f400e
Luiz Guilherme Lins <lins.luizguilherme@gmail.com>
2021-04-07 23:46:23 -0300

    add allowance to the smart contract and downgrade to version ˆ0.6.0

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,13 +1,27 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0-beta.0/contracts/ownership/Ownable.sol";
 
 contract SimpleWallet is Ownable {
+    mapping(address => uint256) public allowance;
+
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(
+            isOwner() || allowance[msg.sender] >= _amount,
+            "You are not allowed!"
+        );
+        _;
+    }
+
     function withdrawMoney(address payable _to, uint256 _amount)
         public
-        onlyOwner
+        ownerOrAllowed(_amount)
     {
         _to.transfer(_amount);
     }
